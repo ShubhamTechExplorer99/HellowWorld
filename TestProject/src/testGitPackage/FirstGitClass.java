037246921ee4826206571e75f811d435ab0f4b1c
Shubham <shubhamjadhav119@gmail.com>
2025-03-17 15:31:10 +0530

    One more priting msg

--- TestProject/src/testGitPackage/FirstGitClass.java
+++ TestProject/src/testGitPackage/FirstGitClass.java
@@ -5,6 +5,8 @@ public class FirstGitClass {
 	public static void main(String[] args) {
 		
 		System.out.println("Git class");
+		
+		System.out.println("One more priting sms will be camed");
 	}
 
 }
